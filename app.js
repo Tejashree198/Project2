adabe902ef4c41e3416f41cc72742ea14450d843
Tejashree Jagtap <tejashreejagtap198@gmail.com>
2025-01-15 19:31:50 +0530

    commit

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ document.querySelectorAll('.card-text').forEach((text) => {
     }
   });
   
+
+  let bucketCounter = localStorage.getItem('bucketCounter') ? parseInt(localStorage.getItem('bucketCounter')) : 0;
+
+  function updateBucketCounter() {
+    const bucketIcon = document.querySelector('.bucket-icon');
+    const bucketCounterSpan = bucketIcon.querySelector('.bucket-counter');
+    bucketCounterSpan.textContent = bucketCounter;
+    localStorage.setItem('bucketCounter', bucketCounter);
+  }
+
+  function addItemToBucket() {
+    bucketCounter++;
+    updateBucketCounter();
+    // Add item to bucket logic here
+  }
+
+  updateBucketCounter();
